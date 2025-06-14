6a90aebb9cef8c1c89c9f5660ac36de650a2b26b
Samual <samual.j.thor@gmail.com>
2025-06-14 15:45:49 -0500

    impl explore command

--- internal/types.go
+++ internal/types.go
@@ -19,36 +19,114 @@ type CliState struct {
 }
 
 type CliEvent struct {
-	Command CliCommand
-	Page    int
-	Output  string
+	Command     CliCommand
+	CommandArgs []string
+	Page        int
+	Output      string
 }
 
 type CliCommand struct {
 	Name        string
 	Description string
-	Callback    func(*CliState) (string, error)
+	Callback    func(*CliState, []string) (string, error) // accepts current state and command arguments
 }
-
-// type DataLoad struct {
-// 	LocationAreaData []LocationArea `json:"location_area_data"`
-// }
 
 // data types
 
 type LocationArea struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	GameIndex int    `json:"game_index"`
-	// EncounterMethodRates []EncounterMethodRate `json:"encounter_method_rates"`
-	// Location             NamedAPIResource      `json:"location"`
-	// Names                []Name               `json:"names"`
-	// PokemonEncounters    []PokemonEncounter   `json:"pokemon_encounters"`
+	ID                   int                   `json:"id"`
+	Name                 string                `json:"name"`
+	GameIndex            int                   `json:"game_index"`
+	EncounterMethodRates []EncounterMethodRate `json:"encounter_method_rates"`
+	Location             Location              `json:"location"`
+	Names                []Name                `json:"names"`
+	PokemonEncounters    []PokemonEncounter    `json:"pokemon_encounters"`
+}
+
+type EncounterMethodRate struct {
+	EncounterMethod EncounterMethod `json:"encounter_method"`
+	VersionDetails  []VersionDetail `json:"version_details"`
+}
+
+type VersionDetail struct {
+	Rate    int     `json:"rate"`
+	Version Version `json:"version"`
+}
+
+type Location struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Name struct {
+	Name     string   `json:"name"`
+	Language Language `json:"language"`
+}
+
+type Language struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	URL      string `json:"url"`
+	Official bool   `json:"official"`
+	ISO639   string `json:"iso639"`
+	ISO3166  string `json:"iso3166"`
+}
+
+type PokemonEncounter struct {
+	Pokemon        Pokemon                  `json:"pokemon"`
+	VersionDetails []VersionEncounterDetail `json:"version_details"`
+}
+
+type Pokemon struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type VersionEncounterDetail struct {
+	Version          Version     `json:"version"`
+	MaxChance        int         `json:"max_chance"`
+	EncounterDetails []Encounter `json:"encounter_details"`
+}
+
+type Version struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Encounter struct {
+	MinLevel        int                       `json:"min_level"`
+	MaxLevel        int                       `json:"max_level"`
+	ConditionValues []EncounterConditionValue `json:"condition_values"`
+	Chance          int                       `json:"chance"`
+	Method          EncounterMethod           `json:"method"`
+}
+
+type EncounterMethod struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	URL   string `json:"url"`
+	Order int    `json:"order"`
+}
+
+type EncounterConditionValue struct {
+	ID        int                `json:"id"`
+	Name      string             `json:"name"`
+	URL       string             `json:"url"`
+	Condition EncounterCondition `json:"condition"`
+}
+
+type EncounterCondition struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
 // api service types
 
-type LocationAreaAPIResponse struct {
+type LocationAreasAPIResponse struct {
 	Count    int            `json:"count"`
 	Next     string         `json:"next"`
 	Previous string         `json:"previous"`
